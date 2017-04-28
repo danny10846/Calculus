03740658280dd1cef594485a1665842dcfeffd4f
User <User@NZXT_Phantom.home>
2017-04-28 14:41:25 +0100

    Added polynomial class functionality to clean up main method

--- caclulus.java
+++ caclulus.java
@@ -6,36 +6,26 @@ public class caclulus{
 	public static void main(String[] args){
 		
 		//variables we'll be using
-		int coefficient;
+		
 		String polynomial;
-		int power, calculus;
+		int calculus;
+		
 		
 		Scanner input = new Scanner(System.in);
 		
 		System.out.println("Please enter your polynomial in the form: '5x^2' for example");
 		polynomial = input.nextLine();
+		
 		System.out.println("Please choose 1 for differentiate and 2 for integrate");
 		calculus = Integer.parseInt(input.nextLine());
 		
-		//split user input into coefficient and power
-		String[] split = polynomial.split("\\^");
-		String pt1 = split[0];
-		String pt2 = split[1];
-		
-		//split pt1 further into coefficient and letter
-		String[] split2 = pt1.split("(?<=\\d)(?=\\D)");
-		String tempCoefficient = split2[0];
-		String algebraLetter = split2[1];
-		
-		//retrieve from array and turn to ints
-		coefficient = Integer.parseInt(tempCoefficient);
-		power = Integer.parseInt(pt2);
+		polynomial givenPoly = new polynomial(polynomial);
 		
 		//calculate the differential
-		int resultCoefficient = power*coefficient;
-		int resultPower = power -1;
+		int resultCoefficient = givenPoly.returnPower()*givenPoly.returnCoefficient();
+		int resultPower = givenPoly.returnPower() -1;
 		
-		String answer = (resultCoefficient + algebraLetter + "^"+ resultPower);
+		String answer = (resultCoefficient + givenPoly.returnAlgebra() + "^"+ resultPower);
 		
 		System.out.println(answer);
 		
---
+++ polynomial.java
@@ -0,0 +1,35 @@
+
+public class polynomial {
+	
+	private int coefficient;
+	private int power;
+	private String algebra;
+	
+	public polynomial(String polynomial){
+		
+		//split user input into coefficient and power
+		String[] split = polynomial.split("\\^");
+		String pt1 = split[0];
+		String pt2 = split[1];
+				
+		//split pt1 further into coefficient and letter
+		String[] split2 = pt1.split("(?<=\\d)(?=\\D)");
+		coefficient = Integer.parseInt(split2[0]);
+		algebra = split2[1];
+				
+		//retrieve from array and turn to ints
+		power = Integer.parseInt(pt2);
+	}//polynomial
+	
+	public int returnCoefficient(){
+		return coefficient;
+	}//returnCoefficient
+	
+	public int returnPower(){
+		return power;
+	}//returnPower
+	
+	public String returnAlgebra(){
+		return algebra;
+	}
+}
